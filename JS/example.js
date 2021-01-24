9357efddbc3acd4662ec0a4a54d8257a44ca18e6
Huseyin Simsek <hsynsimsek723@gmail.com>
2021-01-24 10:59:30 +0300

    Update example.js

--- JS/example.js
+++ JS/example.js
@@ -7,3 +7,9 @@ strangerThings.shift(); // dizinin başından elemanı çıkartır.
 console.log(strangerThings);
 
 console.log(strangerThings.indexOf('Jonathan')); // return -1 çünkü dizide eleman yok
+
+const add= function(a,b){
+  const arg= arguments;
+  return a+b;
+}
+add(2,5);  // arg= [2,5];
